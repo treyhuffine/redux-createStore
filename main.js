a25439612cb596ab4e5f38825ae5a9aa22877cb8
Trey Huffine <treyhuffine@gmail.com>
2016-01-07 23:31:51 -0800

    Add React

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 // import { createStore } from "redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const counter = (state = 0, action) => {
   switch (action.type) {
@@ -36,16 +38,34 @@ const createStore = (reducer) => {
   return { getState, dispatch, subscribe };
 }
 
+const Counter = ({value, onIncrement, onDecrement}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+);
+
 const store = createStore(counter);
 
 const render = () => {
-  console.log(store, store.subscribe);
-  document.body.innerText = store.getState();
+  ReactDOM.render (
+    <Counter
+      value={store.getState()}
+      onIncrement={() =>
+        store.dispatch({
+          type: 'INCREMENT'
+        })
+      }
+      onDecrement={() =>
+        store.dispatch({
+          type: 'DECREMENT'
+        })
+      }
+     />,
+    document.getElementById('root')
+  )
 }
 
 store.subscribe(render);
 render();
-
-document.addEventListener('click', () => {
-  store.dispatch({type: 'INCREMENT'})
-});
